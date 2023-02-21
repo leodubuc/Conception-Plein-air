4638f24db7b849c20559839d480b72bb9f532563
Léo Dubuc <leodubucd@gmail.com>
2023-02-21 15:30:08 -0500

    réglage bug pour les background en php

--- theme_ltdp/footer.php
+++ theme_ltdp/footer.php
@@ -3,58 +3,67 @@
         <div class="nav_footer">
             <nav>
                 <ul>
-                    <li><a href="#">À propos</a></li>
-                    <li><a href="#">Nos services</a></li>
-                    <a href="index.html"><img src="/pfe/ltdp/wp-content/themes/theme_ltdp/assets/images/logo.png" alt="Logo de l'organisme"></a>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Fiches pratiques</a></li>
+                    <?php wp_nav_menu(array(
+                            'theme_location' => 'menu_gauche',
+                            'container' => 'ul',
+                            'container_class' => 'nav-primary__item',
+                        )); ?>
+
+                    <a href="<?php bloginfo('url'); ?>">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/images/logo.png" alt="">
+                    </a>
 
                     <?php wp_nav_menu(array(
-                        'theme_location' => 'menu_principal',
-                        'container' => 'ul',
-                    )); ?>
+                            'theme_location' => 'menu_gauche',
+                            'container' => 'ul',
+                            'container_class' => 'nav-primary__item',
+                        )); ?>
                 </ul>
             </nav>
         </div>
 
+
+<?php if(have_rows('footer_coordonnees', 'options')) : ?>   
+<?php while (have_rows('footer_coordonnees', 'options')) : the_row() ?>   
+
+    <?php
+    $contact = get_field('footer_contact');
+    if( $contact ): ?>
         <div class="coordonees">
             <div class="contact">
-                <span>Contact</span>
+                <?php the_field('footer_contact_titre') ?>
                 <div>
                     <div class="list-contact phone">
-                        <?php if(have_rows('footer_contact', 'options')) : ?>   
-                                <?php while (have_rows('footer_contact', 'options')) : the_row() ?>   
-                                    <?php $img = get_sub_field('icon_contact') ?>
+                        <?php if(have_rows('footer_contact_list', 'options')) : ?>   
+                                <?php while (have_rows('footer_contact_list', 'options')) : the_row() ?>   
+                                    <?php $img = get_sub_field('footer_contact_list_icon') ?>
                                     <?php $url = $img['url'] ?>
-                                <a href="<?php echo get_sub_field('info_contact')?>" target="_blank"><img src="<?php echo esc_url($url); ?>" alt="Logos de réseaux sociaux"></a>
+                                <a href="<?php echo get_sub_field('footer_contact_list_link')?>" target="_blank"><img src="<?php echo esc_url($url); ?>" alt="Coordonée du Temps d'une pause"></a>
                                 <?php endwhile; ?>
                         <?php endif; ?> 
                     </div>
-                    <div class="list-contact mail">
-                        <img src="/pfe/ltdp/wp-content/themes/theme_ltdp/assets/images/footer/mail.png" alt="Icon de e-mail">
-                        <a href="mailto: admissions@tempsdunepause.org">admissions@tempsdunepause.org</a> 
-                    </div>
-
-                    <div class="list-contact map">
-                        <img src="/pfe/ltdp/wp-content/themes/theme_ltdp/assets/images/footer/pointer.png" alt="Icon de pointeur">
-                        <a href="https://goo.gl/maps/tzHtqSvpB76DQwwP7" target="_blank">Où nous trouver?</a> 
-                    </div>
                 </div>
             </div>
+    <?php endif; ?>        
 
+
+    <?php
+    $reseaux = get_field('footer_socials');
+    if( $reseaux ): ?>
             <div class="medias">
-                <span>Réseaux Sociaux</span>
-                <div class="reseaux">
-                    <?php if(have_rows('footer_socials', 'options')) : ?>   
-                            <?php while (have_rows('footer_socials', 'options')) : the_row() ?>   
-                                <?php $img = get_sub_field('image_socials') ?>
-                                <?php $url = $img['url'] ?>
-                            <a href="<?php echo get_sub_field('url_socials')?>" target="_blank"><img src="<?php echo esc_url($url); ?>" alt="Logos de réseaux sociaux"></a>
-                            <?php endwhile; ?>
-                    <?php endif; ?>  
+                <?php the_field('footer_socials_titre') ?>
+                <div class="reseaux">  
+
+                <?php if(have_rows('footer_socials_list', 'options')) : ?>   
+                    <?php while (have_rows('footer_socials_list', 'options')) : the_row() ?>   
+                        <?php $img = get_sub_field('footer_socials_list_icon') ?>
+                        <?php $url = $img['url'] ?>
+                        <a href="<?php echo get_sub_field('footer_socials_list_link')?>" target="_blank"><img src="<?php echo esc_url($url); ?>" alt="Logos de réseaux sociaux"></a>   
+                    <?php endwhile; ?>
+                <?php endif; ?> 
                 </div>
             </div>
-
+    <?php endif; ?>      
               
 
             <div class="infolettre">
@@ -71,6 +80,10 @@
                 
             </div>
         </div>
+
+<?php endwhile; ?>
+<?php endif; ?>  
+
 
         <div class="partenaires">
             <?php if(have_rows('footer_partenaire', 'options')) : ?>   
--- theme_ltdp/functions.php
+++ theme_ltdp/functions.php
@@ -13,7 +13,11 @@ add_theme_support( 'title-tag' );
 
 // Menu
 function pw_creer_menu() {
-	register_nav_menu('menu_principal', 'Menu principal');
+	register_nav_menus( array(
+		'menu_principale' => __( 'Menu principale', 'text_domain'),
+		'menu_gauche' => __( 'Menu gauche', 'text_domain'),
+		'menu_droite' => __( 'Menu droite', 'text_domain'),
+	) );
 }
 add_action('init', 'pw_creer_menu');
 
