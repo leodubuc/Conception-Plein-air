bc7fc148a280502b4ed3d7df8bdfcd1a9f7d722c
Léo Dubuc <leodubucd@gmail.com>
2023-02-23 12:01:57 -0500

    code pour le header qui sert a rien

--- src/scripts/components/Header.js
+++ src/scripts/components/Header.js
@@ -9,6 +9,18 @@ export default class Header {
 
     this.init();
     this.initNavMobile();
+
+    let overlay = document.querySelector('.menu_overlay');
+    let open = document.querySelector('.fa-bars');
+    let close = document.querySelector('.fa-times');
+
+    open.addEventListener('click', () => {
+      overlay.style.transform = 'translateY(0)';
+    });
+
+    close.addEventListener('click', () => {
+      overlay.style.transform = 'translateY(-110%)';
+    });
   }
 
   init() {
