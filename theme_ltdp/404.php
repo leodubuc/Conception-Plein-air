adfb681d5eaf12195b5d3669aff9ff7a81bcb1d7
Marie-Pier Hébert <hebertmarie44@gmail.com>
2023-02-28 10:24:15 -0500

    Page 404 HTML + CSS + PHP (non dynamique)

--- theme_ltdp/404.php
+++ theme_ltdp/404.php
@@ -1,3 +1,17 @@
-<?php get_header(); ?>
+<?php /* Template Name: 404 */ ?>
 
+<?php get_header(); ?>
+<section class="erreur">
+    <div class="wrapper">
+        <div class="content-left">
+            <h1 data-scrolly="fromBottom">404</h1>
+
+            <p data-scrolly="fromBottom">Cette page est inexistante.</p>
+
+            <a href="https://mhebert.dectim.net/pfe/ltdp/" data-scrolly="fromBottom">Revenir à l’accueil</a>
+        </div>
+
+        <img data-scrolly="fromBottom" src="https://mhebert.dectim.net/pfe/ltdp/wp-content/themes/theme_ltdp/assets/images/illustrations/404.svg" alt="Illustration 404" />
+    </div>
+</section>
 <?php get_footer(); ?>
